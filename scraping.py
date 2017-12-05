70dab8a1e96cd26015f2fa8ba1214fab9c382a0b
kumapana <kumachan05-11@hotmail.co.jp>
2017-12-05 16:04:46 +0900

    一応完成

---
+++ scraping.py
@@ -0,0 +1,18 @@
+from urllib import request
+from bs4 import BeautifulSoup as BS
+
+
+def main():
+    url = "http://imotosae.com/news/"
+    req = request.Request(url)
+    res = request.urlopen(req)
+    print(res)
+    html = res.read()
+    soup = BS(html, "lxml")
+    topics = soup.find_all('h1', 'c-thumb-index__title')
+    for i in range(len(topics)):
+        print(topics[i].string, "\n")
+
+
+if __name__ == '__main__':
+    main()
